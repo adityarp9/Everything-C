05fce050afa2aee4812d35302f31162e7a603053
Aditya R. Patgaonkar <adityapatgaonkar9@gmail.com>
2017-09-08 21:18:26 +0530

    CodeChef problems, solved.

---
+++ Solving & Learning/CodeChef/entanceExam.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int n;
+scanf("%d", &n);
+int main(void)
+{
+    /*
+    printf("Enter the number of students:");
+    printf("%d", n);
+
+    typedef struct student
+    {
+        int sNo, marks[n], totalScore;
+    }*/
+    return 0;
+}
